ac7a47936a26048ae2c4b445cc84c5c4eb6d7aa6
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-10-25 12:39:08 +0800

    Day17.java

---
+++ day17.java
@@ -0,0 +1,17 @@
+package day17;
+
+public class day17 {
+    
+    public static void main(String[] args) {
+        String a,b,c;
+        a = "125000";
+        b ="125000";
+        c ="127";
+        
+        int d =Integer.parseInt(a);
+        double e = Double.parseDouble(b);
+        byte f = Byte.parseByte(c);
+        System.out.println("nilai integer\t: "+d+"\nNilai byte\t: "+f+"\nNilai double\t: "+e);
+    }
+    
+}
