4ad0c4362d90018ae7d734b53c731553e42af29b
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-10-27 09:50:53 +0800

    day19.java
    deklarasi variabel
    inisialisasi variabel
    update variabel

---
+++ day19.java
@@ -0,0 +1,29 @@
+package seratushari;
+
+public class day19 {
+
+    public static void main(String[] args) {
+        //Deklarasi variabel
+        String alamat,noWa;
+        byte jumlah_saudara;
+        double ipk;
+
+
+        // inisialisasi variabel
+        String nama = "Baharuddin";
+        byte umur = 21;
+        float  beratbadan = 56.5f;
+        double tinggibdn = 70.0;
+        char st = 'L';
+        System.out.println("Nama saya adalah "+nama+"\nSaya berusia "+umur+"th"+"\nBerat badan "+beratbadan+"kg"+"\nTinggi "+tinggibdn+"cm"+"\nJenis kelain "+st+"\n=============================");
+
+        //Update variabel
+        alamat = "Lagi agi ,Campalagian,Polewali mandar";
+        noWa = "082192445687";
+        jumlah_saudara = 3;
+        ipk = 3.9999999;
+        System.out.println("No wa\t\t : "+noWa+"\nAlamat\t\t : "+alamat+"\nJumlah saudara\t : "+jumlah_saudara+"\nBismilah ipk\t : "+ipk);
+
+
+    }
+}
