56601b81ab247c537bff3fcd5692edee7698f86e
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-10-28 12:04:45 +0800

    day20.java
     : )

---
+++ day20.java
@@ -0,0 +1,22 @@
+
+package seratushari;
+
+// Deklarasi type data final 
+class update{
+    final int a = 100;
+    final byte c = 99;
+}
+
+public class day20 {
+    
+    public static void main(String[] args) {
+        // tinggal memanggil classnya dan variabel nya dengan cara memperbaru
+        
+        update b = new update();
+        update d = new update();
+        System.out.println(b.a+"\n"+d.c);
+     
+        
+    }
+    
+}
