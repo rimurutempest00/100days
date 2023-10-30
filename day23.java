c8d04b14aff50d4eb272309458d139736491e089
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-10-31 07:24:43 +0800

    day23.java

---
+++ day23.java
@@ -0,0 +1,19 @@
+package day23;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 45;
+        a += a;
+       System.out.println(a);
+        a-=20;
+        System.out.println(a);
+        a*=a;
+        System.out.println(a);
+        a/=5;
+        System.out.println(a);
+        a%=5;
+        System.out.println(a+"\nARIGATO :)");
+    }
+    
+}
