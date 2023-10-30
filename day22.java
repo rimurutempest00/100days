5bcf574f6ee0e3d80ba1aebe54d138cc6d2c392a
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-10-30 09:57:40 +0800

    day22.java
    aritmatika dan penugasan

---
+++ day22.java
@@ -0,0 +1,36 @@
+
+package seratushari;
+
+
+public class day22 {
+    public static void main(String[] args) {
+        byte a,b,c;
+        int d,e,f;
+        long g,h,j;
+        double i,k,l;
+        short m,n,o;
+        
+        a = 20 + 50;
+        b = 100 - 30;
+        c = (byte) ((a+b)-23);
+        d = 120 + 180;
+        e = 25 * 25;
+        f = (e-d)*10;
+        g = 2000000 + 3000000;
+        h = 1000000 - 50000;
+        h += g;
+        j = h * 5;
+        i = 12 *100;
+        k = 1800 + 1800;
+        l = (j*k) / 12;
+        m = 10;
+        m *= 20;
+        m +=10;
+        m -= 20;
+        n = 25 + 25;
+        o = (short) ((m+n)-28);
+        System.out.println(c+"\n"+f+"\n"+j+"\n"+l+"\n"+o+"\nSkian dan Terimakasih Semoga Bahagia : )");
+        
+    }
+    
+}
