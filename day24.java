1aba54967c1b74b9cc7549987614d00c9930bbdd
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-01 22:27:07 +0800

    day24.java

---
+++ day24.java
@@ -0,0 +1,11 @@
+
+package seratushari;
+
+public class day24 {
+    public static void main(String[] args) {
+        int a = 25,b = 20,c = a+b;
+        boolean sd =( a == a ),tsd =( c!= a ),lksd =( a <= c ),lbsd =( b >= a ),lk = ( a < b ),lb = ( c > a );
+        System.out.println(sd + "\n"+tsd+"\n"+lksd+"\n"+lbsd+"\n"+lk+"\n"+lb);
+    }
+    
+}
