49e74c5e2abaeb36f5e7b43fbb412424ebb7eedd
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-08 15:02:48 +0800

    day31.java
    ferulangan for yang dimulai dari angka kecil ke angka yang besar sesuai angka yang di imputkan

---
+++ day31.java
@@ -0,0 +1,22 @@
+
+package seratushari;
+
+import java.util.Scanner;
+
+public class day31 {
+    public static void main(String[] args) {
+        Scanner ya = new Scanner (System.in);
+        int awal,akhir;
+        System.out.print("Mulai dari angka : ");
+        awal = ya.nextInt();
+        System.out.print("Sampai angka     : ");
+        akhir = ya.nextInt();
+        
+        for (int i = 0; i <= akhir; i++) {
+            System.out.println(i);
+            
+        }
+      //perulangan for dengan menggunakan metode inputan scanner
+    }
+    
+}
