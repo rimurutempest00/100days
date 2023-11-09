c199978621b5263f9d023de681fd4f8e431199e6
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-09 23:32:05 +0800

    perulangan for dari nilai besar ke kecil setiap kali di ulang bakalan di kurang dengan 2
    bukan kisa kita yang di ulang krn itu sudh menjadi mustahil untuk terjadi

---
+++ day32.java
@@ -0,0 +1,13 @@
+
+package seratushari;
+
+public class day32 {
+    public static void main(String[] args) {
+        for (int i = 20; i > 1; i-=2) {
+            System.out.print(i+" ");
+            
+        }
+        System.out.println("SEMOGA KAMU BAHAGIA BERSAMA DIA :) ");
+    }
+    
+}
