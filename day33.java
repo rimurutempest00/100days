5a99200ec52dd50625fae5feef7f932ecea459b7
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-10 21:07:05 +0800

    perulangan for menggunakan incremen dan decremen
    apakah kisah kita bisa di ulng seperti perulangan for : )

---
+++ day33.java
@@ -0,0 +1,15 @@
+package seratushari;
+public class day33 {
+    public static void main(String[] args) {
+        int angka= 100;
+        //mulai dari angka nol sampai angka 100 akan di tambah dengan angka 2
+        for (int i = 0; i < angka; i+=2) {
+            System.out.println(i);
+            }
+        //ketika sudah samopai mencapai angka 100 maka akan di kurangin dengan anka 2 lagi sampai angka 0
+                for (int j= angka ; j > 0; j-=2) {
+                    System.out.println(j);
+                }
+            
+        }
+}
