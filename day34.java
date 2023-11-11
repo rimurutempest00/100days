82e51dbd1c9b2553790d09b6ad86217e0d70e4f1
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-11 22:14:45 +0800

    Array
    Semangat tingggal 66 hari lagi

---
+++ day34.java
@@ -0,0 +1,11 @@
+package seratushari;
+public class day34 {
+    public static void main(String[] args) {
+        int [] angka = {200,220,230,240,250,260};
+        for (int hasil : angka) {
+            System.out.println("= "+hasil);
+        }
+        System.out.println("SEMOGA BAHAGIA : )");
+    }
+    
+}
