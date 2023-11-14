57c0b230c01fafcba4e5295d60f9a22359f92eaf
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-14 23:15:33 +0800

    arrray

---
+++ day37.java
@@ -0,0 +1,11 @@
+package seratushari;
+public class day37 {
+    public static void main(String[] args) {
+        String [] hari = {"Minggu ","Senin ","Selasa ","Rabu ","Kamis ","Jumat ","Sabtu"};
+        for(String a : hari){
+            System.out.println(a);
+        }
+      System.out.printl("SEMOGA BAHAGIA :)");
+    }
+    
+}
