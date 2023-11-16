b4c04d3463cc22628604f0713cc6ffd8643d551a
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-16 21:30:12 +0800

    array multi dimensi

---
+++ day39.java
@@ -0,0 +1,19 @@
+package seratushari;
+public class day39 {
+    public static void main(String[] args) {
+        // array multi dimensi
+        String [][] buah = {
+            {"Apel","Mangga","Durian\n"},
+            {"Semangka","Jambu","Langsat\n"},
+            {"Jeruk","pepaya","Nanas"}
+        };
+        System.out.print(buah[0][0]+"\n"+buah[0][1]+"\n"+buah[0][2]);
+        System.out.print(buah[1][0]+"\n"+buah[1][1]+"\n"+buah[1][2]);
+        System.out.print(buah[2][0]+"\n"+buah[2][1]+"\n"+buah[2][2]);
+        System.err.println("SEMOGA BAHAGIA :)");
+            
+            
+        }
+    }
+    
+
