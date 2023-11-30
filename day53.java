8388db4cee0ee9b03b4cc6f7356f09288d80a1ff
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-11-30 22:02:20 +0800

    pengiriman data string ke method / fungsi

---
+++ day53.java
@@ -0,0 +1,10 @@
+package seratushari;
+public class day53 {
+    public static void foryou(String text){
+        System.out.println(text);
+    }
+    public static void main(String[] args) {
+        String text = "Sehat selalu buat para mentor : ) ";
+        foryou(text);
+    }
+}
