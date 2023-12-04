012bbdc32104fe2c6358c7ab3ee780225fcc3080
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-04 20:20:42 +0800

    String charAt() fungsinya memanggil satu karakter pd string sesuai index yg dimasukkan.

---
+++ day57.java
@@ -0,0 +1,11 @@
+package day57;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama ="Baharuddin";
+        char karakter = nama.charAt(1);//mengambil karakter ke 2 krn index dimulai dari 0
+        System.out.println("Karakter : "+karakter);
+    }
+    
+}
