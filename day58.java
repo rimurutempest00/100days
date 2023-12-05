cb143a125f4d5147ac237033835b7fd6f8657b06
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-05 22:04:25 +0800

    Konversi huruf besar menjadi huruf kecil

---
+++ day58.java
@@ -0,0 +1,10 @@
+package day58;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "BAHARUDDIN";
+        System.out.println(nama.toLowerCase());//toLowerCase() untuk mengkonversi karakter string menjadi hurup kecil
+    }
+    
+}
