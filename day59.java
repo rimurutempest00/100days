a5a7fbf64be032f7db0220cbfc082239954766c8
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-06 20:02:47 +0800

    mengubah huruf kecil ke huruf besar

---
+++ day59.java
@@ -0,0 +1,14 @@
+package seratushari;
+
+import java.util.Scanner;
+
+public class day59 {
+    public static void main(String[] args) {
+        Scanner up = new Scanner(System.in);
+        System.out.print("Masukan kata : ");
+        String kata = up.nextLine();
+        System.out.println("________________________");
+        System.out.println("kata awal       : "+kata);
+        System.out.println("hasil komversi  : "+kata.toUpperCase()+"\nARIGATO : )");
+    }
+}
