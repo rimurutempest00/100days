9cdbe47d5162ab1b3e2434d969a512ed08236a67
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-07 10:30:43 +0800

    penggunaan contains pada string

---
+++ day60.java
@@ -0,0 +1,12 @@
+package seratushari;
+public class day60 {
+    public static void main(String[] args) {
+        String kata= "Arigato : )";
+        String contains = "Seyyonara ";
+        if (kata.contains(kata)) {
+            System.out.println("String mengandung contains tersebut");
+        }else{
+          System.out.println("String tidak mengandung coontains tersebut");  
+        }
+    }
+}
