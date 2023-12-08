017173b29e247d1307ffcbfbaff0bbe84fd20528
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-08 13:34:21 +0800

    Menggabungkan 2 variabel

---
+++ day61.java
@@ -0,0 +1,12 @@
+package day61;
+
+public class day61 {
+    
+    public static void main(String[] args) {
+        String depan, belakang;
+        depan = "Bahar";
+        belakang ="Offical";
+        System.out.println(depan+" "+belakang);
+    }
+    
+}
