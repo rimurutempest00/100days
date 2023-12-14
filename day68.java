cfbf9b43b1d08423495a8fb58627d38150ad8891
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-15 00:02:59 +0800

    Hanya bisa membalikqn kata klw dia gak boleh

---
+++ day68.java
@@ -0,0 +1,11 @@
+package day68;
+
+public class day68 {
+    
+    public static void main(String[] args) {
+        String nama = "niddurahaB";
+        String balik= new StringBuilder(nama).reverse().toString();
+        System.out.println("String awal: " + nama+"\nSetelah di balik : "+balik);
+    }
+    
+}
