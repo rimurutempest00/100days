0dd18bb27d860890548cc3101138f709b33b9330
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-19 21:44:42 +0800

    prosedur dan fungsi

---
+++ day72.java
@@ -0,0 +1,22 @@
+
+package seratushari;
+
+
+public class NewClass1 {
+public static String siswa(){
+String nama = "Baharuddin";
+    return nama;
+  
+}
+public static void aku(String alamat){
+    int umur = 21;
+    System.out.println("Usia\t: "+umur+" th\n"+"Alamat\t: "+alamat);
+    
+}
+    public static void main(String[] args) {
+        int saya = 20;
+        
+        System.out.println("Nama\t: "+siswa());
+        aku("Campalagian");
+    }
+}
