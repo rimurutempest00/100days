833147f8e710e1006513fd6c7a92d07aaa310dbc
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2023-12-27 20:05:31 +0800

    menggunakan LinkedList

---
+++ day80.java
@@ -0,0 +1,18 @@
+package seratushari;
+
+import java.util.LinkedList;
+
+public class day80 {
+    public static void main(String[] args) {
+        LinkedList <String> dataBuah = new LinkedList <>();
+        dataBuah.add("mangga");
+        dataBuah.add("apel");
+        dataBuah.add("durian");
+        dataBuah.add("semangka");
+        int jumlah = dataBuah.size();
+        for (String nam : dataBuah) {
+            System.out.println(nam);
+            
+        }
+        }
+    }
