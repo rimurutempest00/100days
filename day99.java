830188d2e855bb10f228f983cc82b72c34d80c41
Bahar Offical <147222847+rimurutempest00@users.noreply.github.com>
2024-01-15 20:46:02 +0800

    Type data primitif

---
+++ day99.java
@@ -0,0 +1,13 @@
+package day99;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Biodata Palsu");
+        int tahun = 2025;
+        char huruf ='p';
+        float bb = 3.45f;
+        boolean banding = "aku"=="dia";
+        System.out.println("tahun sekaran\t: "+tahun+"\njenis kelamin\t: "+huruf+"\nberat badan\t: "+bb+"\naku sama dia?\t: "+banding);
+    }  
+}
